44e83774aba384d632f13678d9c36248cf10c721
Maria <maria.kozhina@dataart.com>
2019-12-10 19:12:57 +0300

    fix catalog

--- gulpfile.js
+++ gulpfile.js
@@ -86,6 +86,11 @@ gulp.task("clean", function () {
  return del("build");
 });
 
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
 gulp.task("server", function () {
   server.init({
     server: "build/"
@@ -94,11 +99,6 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
-});
-
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
 });
 
 gulp.task("build", gulp.series(
